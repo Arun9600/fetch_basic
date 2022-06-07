95ab556e558a4a4e1bc66ffacd91bb3c44cf6e7a
Arun <69082994+Arun9600@users.noreply.github.com>
2022-06-07 16:24:49 +0530

    Changed api data's by default instead of clicking button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Fetchbasic from './Fetchbasic';
 const App = () => {
   return (
     <>
-    <h1>Json Placeholder API users in json format</h1>
+    
     <Fetchbasic />
     </>
   );
--- src/Fetchbasic.js
+++ src/Fetchbasic.js
@@ -2,16 +2,13 @@ import { useState } from "react";
 
 const Fetchbasic = () =>{
 
-  const buttonColor = {backgroundColor:'green',color:'#fff',padding:'10px 15px',borderRadius:'6px',border:'none',cursor:'pointer',marginLeft:'20px'}
-
-  const [datas, setDatas] = useState();
- const getAPI = () => {fetch('https://jsonplaceholder.typicode.com/users')
+  const [datas, setDatas] = useState(
+    fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
-  .then((data) => {setDatas(data)})};
-
+  .then((data) => {setDatas(data)})
+  );
   return(
       <>
-      <button onClick={getAPI} style={buttonColor}>Click to get user list</button>
       <pre>{JSON.stringify(datas,null,2)}</pre>
       </>
 
